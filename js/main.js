d429a5d91be4f9bd931331e3a1c08cc6c916b615
David Valero <davidvalero17@hotmail.com>
2023-02-01 09:22:09 +0100

    primer reto hecho

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let numberOne = 12;
+let numberTwo = 12;
+
+if (numberOne < numberTwo){
+    console.log(numberOne, "es el menor de los dos")
+} else if (numberOne > numberTwo){
+    console.log(numberOne, "es el mayor de los dos")
+}else {
+    console.log("Ambos números son iguales")
+}
